68bf499b6a9f989a61ecc8da44ff3c22f97fbf7e
erlend oksvoll <erlend.oksvoll@digdir.no>
2024-11-07 13:16:26 +0100

    corrected scope requirement for duediligence

--- src/Altinn.Dan.Plugin.Statensvegvesen/Metadata.cs
+++ src/Altinn.Dan.Plugin.Statensvegvesen/Metadata.cs
@@ -81,7 +81,7 @@ public class Metadata : IEvidenceSourceMetadata
                 {
                     new MaskinportenScopeRequirement
                     {
-                        RequiredScopes = new List<string> { "altinn:dataaltinnno/eduediligence" }
+                        RequiredScopes = new List<string> { "altinn:dataaltinnno/duediligence" }
                     }
                 }
             },
