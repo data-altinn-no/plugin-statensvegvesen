1d726a44f2983431d3955b8f4dd1718b2e577a0d
PERHOF <pernille.hofgaard@hotmail.com>
2026-01-02 11:03:38 +0100

    added spaced around equality operator and removed unused usings

--- Altinn.Dan.Plugin.Statensvegvesen.Test/TestHelpers/TestHelpers.cs
+++ Altinn.Dan.Plugin.Statensvegvesen.Test/TestHelpers/TestHelpers.cs
@@ -2,7 +2,6 @@ using System;
 using System.IO;
 using System.Net;
 using System.Net.Http;
-using System.Threading;
 using System.Threading.Tasks;
 using FakeItEasy;
 using Newtonsoft.Json;
@@ -40,7 +39,7 @@ namespace Altinn.Dan.Plugin.Statensvegvesen.Test.TestUtils
 
             // Configure the fake to throw an exception
             A.CallTo(handler)
-                .Where(call => call.Method.Name=="SendAsync")
+                .Where(call => call.Method.Name == "SendAsync")
                 .WithReturnType<Task<HttpResponseMessage>>()
                 .Throws(new HttpRequestException("Connection refused"));
 
